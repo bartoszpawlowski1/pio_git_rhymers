2196eaa979c283937070c95b10a5dbc84ecb2438
Bartosz Pawlowski <235949@edu.p.lodz.pl>
2022-05-10 13:50:07 +0200

    Dodanie dokumentacji

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -8,30 +8,58 @@ public class IntLinkedList {
 		private Node prev;
 		private Node next;
 
+		/**
+		 * Konstruktor ustawiajacy wartosc wezla
+		 * @param i nowa wartosc wezla
+		 */
 		public Node(int i) {
 			setValue(i);
 		}
 
+		/**
+		 * Metoda umozliwiajaca pobranie wartosci wezla
+		 * @return wartosc wezla
+		 */
 		public int getValue() {
 			return value;
 		}
 
+		/**
+		 * Metoda umozliwiajaca ustawienie wartosci wezla
+		 * @param value nowa wartosc wezla
+		 */
 		public void setValue(int value) {
 			this.value = value;
 		}
 
+		/**
+		 * Pobranie poprzedniego wezla
+		 * @return porzedni wezel
+		 */
 		public Node getPrev() {
 			return prev;
 		}
 
+		/**
+		 * Ustawienie poprzedniego wezla
+		 * @param prev nowy wezel
+		 */
 		public void setPrev(Node prev) {
 			this.prev = prev;
 		}
 
+		/**
+		 * Pobranie nastepnego wezla
+		 * @return nastepny wezel
+		 */
 		public Node getNext() {
 			return next;
 		}
 
+		/**
+		 * Ustawienie nastepnego wezla
+		 * @param next nowy wezel
+		 */
 		public void setNext(Node next) {
 			this.next = next;
 		}
@@ -41,6 +69,10 @@ public class IntLinkedList {
 	private Node last;
 	private int i;
 
+	/**
+	 * Metoda umozliwiajaca wstawienie nowego wezla do listy
+	 * @param i wartosc nowego wezla
+	 */
 	public void push(int i) {
 		if (getLast() == null) {
 			setLast(new Node(i));
@@ -52,14 +84,26 @@ public class IntLinkedList {
 		}
 	}
 
+	/**
+	 * Metoda sprawdzajaca czy lista jest pusta
+	 * @return true jest lista jest pusta, false jesli znajduja sie w niej elementy
+	 */
 	private boolean isEmpty() {
 		return getLast() == null;
 	}
 
+	/**
+	 * Metoda sprawdzajaca czy lista jest pelna
+	 * @return false
+	 */
 	public boolean isFull() {
 		return false;
 	}
 
+	/**
+	 * Metoda umozliwiajaca pobranie wartosci ostatniego wezla
+	 * @return wartosc ostatniego wezla na liscie
+	 */
 	public int top() {
 		if (isEmpty()) {
 			return -1;
@@ -67,6 +111,10 @@ public class IntLinkedList {
 		return getLast().getValue();
 	}
 
+	/**
+	 * Metoda umozliwiajaca usuniecie ostatniego wezla z listy
+	 * @return wartosc usunietego wezla
+	 */
 	public int pop() {
 		if (isEmpty()) {
 			return -1;
@@ -76,15 +124,19 @@ public class IntLinkedList {
 		return ret;
 	}
 
+	/**
+	 * Pobranie ostatniego wezla z listy
+	 * @return ostatni wezel z listy
+	 */
 	public Node getLast() {
 		return last;
 	}
 
+	/**
+	 * Ustawienie ostatniego wezla na liscie
+	 * @param last nowy wezel
+	 */
 	public void setLast(Node last) {
 		this.last = last;
-	}
-
-	public int getI() {
-		return i;
 	}
 }
